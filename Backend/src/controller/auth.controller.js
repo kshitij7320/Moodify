385d46e225df6b2d0f936d89d192a467b5c36c52
Kshitij Himanshu <kshitij7320@gmail.com>
2026-06-06 00:38:13 +0530

    login api created with Controller

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -3,7 +3,85 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 async function registerUser(req,res){
+ const {username ,email ,password} = req.body
+ 
+ const isAlreadyRegister = await userModel.findOne({
+    $or:[
+        {email},
+        {username}
+    ]
+ })
+ if(isAlreadyRegister){
+    return res.status(400).json({
+        message:"User with the same username or email is already exists"
+    })
+ }
+ const hash = await bcrypt.hash(password, 10);
 
+ const user = await userModel.create({
+    username,
+    email,
+    password:hash
+ })
+
+ const token = jwt.sign({
+    id: user._id,
+    username: user.username
+ },process.env.JWT_SECRET,
+{
+    expiresIn: "3d"
+})
+
+res.cookie("token", token)
+return res.status(200).json({
+    message:"User registered successfully",
+    user:{
+        id: user._id,
+        email: user.email,
+        username: user.username
+    }
+})
 }
 
-module.exports = { registerUser }
+async function loginUser(req,res){
+    const { email,username,password} = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            {email},
+            {username}
+        ]
+    })
+    if(!user){
+        return res.status(400.json({
+            message: "Invalid crediential"
+        }))
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message: "invalid password"
+        })
+    }
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+    },process.env.JWT_SECRET,
+{
+    expiresIn: "3d"
+})
+
+res.cookies("token", token)
+
+return res.status(200).json({
+    message:"User successfully Login",
+    user:{
+        id: user._id,
+        username: user.username,
+        email: user.email
+    }
+})
+}
+
+module.exports = { registerUser, loginUser }
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require("../controller/auth.controller")
 const router = express.Router()
 
 router.post("/register", authController.registerUser)
+router.post("/login", authController.loginUser)
 
 module.exports = router
