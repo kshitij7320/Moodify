c12f14277c3dec649a67a660104ee156e41540ac
Kshitij Himanshu <kshitij7320@gmail.com>
2026-06-07 20:40:06 +0530

    made changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,17 +2,23 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const connectdb = require("./config/db/database")
 const authRoutes = require("./routes/auth.routes")
+const cors = require("cors")
 
 connectdb();
 
 
 
+
+
 const app = express();
 app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
+
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
-
-
 
 
 
