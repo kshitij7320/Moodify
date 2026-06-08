0d86945aa114e3d2ee796dae16a77be922107f9d
Kshitij Himanshu <kshitij7320@gmail.com>
2026-06-09 01:13:20 +0530

    added middleware for upload file

--- Backend/src/controller/song.controller.js
+++ Backend/src/controller/song.controller.js
@@ -1,5 +1,20 @@
 const songModel = require("../models/song.model")
+const id3 = require("node-id3")
+const storageService = require("../services/storage.service")
 
 async function uploadSong(req,res){
-    
+    const tags = id3.read(req.file.buffer)
+    const songFile = await storageService.uploadFile({
+        buffer:songBuffer,
+        filename: tags.title,
+        folder: "/Moodify/songs"
+    })
+
+    const posterFile = await storageService.uploadFile({
+        buffer:tags.image.imageBuffer,
+        
+    })
+
 }
+
+module.exports = {uploadSong}
--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const upload = require("../middlewares/upload.middleware")
+const songController = require("../controller/song.controller")
 
 const router = express.Router()
 
-router.post("/", upload.single("song"), )
+router.post("/", upload.single("song"), songController.uploadSong)
 
 module.exports = router
